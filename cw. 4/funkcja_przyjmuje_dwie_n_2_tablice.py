94a048ffe1fe4f3d314291f5a4ce9056a2f3660b
mpyrek <82597389+mpyrek@users.noreply.github.com>
2021-09-22 14:37:13 +0200

    Delete cw. 4 directory

--- cw. 4/funkcja_przyjmuje_dwie_n_2_tablice.py
+++
@@ -1,2 +0,0 @@
-#Proszę zaimplementować funkcję void SumSort(int A[], int B[], int n). Funkcja ta przyjmuje na wejściu dwie n2-elementowe tablice (A i B) i zapisuje w tablicy B taką permutację elementów z A, że:
-
